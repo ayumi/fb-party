9049283c15d1d00ccfacdeee0f1cd9ea46d35013
Ayumi Yu <ayumi@ayumiyu.com>
2015-04-26 23:12:25 -0700

    Toggle with button

--- src/js/trollio.js
+++ src/js/trollio.js
@@ -2,23 +2,40 @@ class Trollio {
   constructor() {
     console.log("Initializing Troll.io Extension");
 
-    let iframe = document.createElement("iframe");
-    iframe.id = "trollio-frame";
+    this.visible = true;
+
+    // Insert iframe
+    this.iframe = document.createElement("iframe");
+    this.iframe.id = "trollio-frame";
 
     let pageHref = encodeURIComponent( location.href );
     let frameBaseUrl = chrome.extension.getURL("frame/frame.html");
-    iframe.src = `${frameBaseUrl}?q=${pageHref}`;
+    this.iframe.src = `${frameBaseUrl}?q=${pageHref}`;
     // iframe.src = frameBaseUrl;
 
-    document.body.appendChild(iframe);
+    document.body.appendChild( this.iframe );
+  }
+
+  toggle() {
+    this.visible = !( this.visible );
+    if (this.visible) {
+      this.iframe.className = this.iframe.className.replace( /\bhidden\b/, '' );
+    } else {
+      this.iframe.classList.add('hidden');
+    }
   }
 }
 
 var trollio = null;
+var visible = false;
 
 
 function trollioActivator(request, sender, sendResponse) {
-  trollio = new Trollio();
+  if (trollio == null) {
+    trollio = new Trollio();
+  } else {
+    trollio.toggle();
+  }
 }
 
 chrome.runtime.onMessage.addListener(trollioActivator);
